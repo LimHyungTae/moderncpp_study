577353d93e9c585bf32c1776ab1ff7ce6e607dbe
Hyungtae Lim <shapelim@mit.edu>
2024-05-11 18:11:57 -0400

    Upload files

--- src/stl_examples/std_for_each.cpp
+++ src/stl_examples/std_for_each.cpp
@@ -4,9 +4,23 @@
 
 int main() {
     std::vector<int> v = {1, 2, 3, 4, 5};
-    std::for_each(v.begin(), v.end(), [](int &x) { x *= 2; });
+
+    for (int i = 0; i < v.size(); ++i) {
+        v[i] *= 2;
+    }
+
+    // 출력: 2 4 6 8 10
     for (auto i : v) {
         std::cout << i << " ";
     }
+    std::cout << std::endl;
+
+    v = {1, 2, 3, 4, 5};
+    std::for_each(v.begin(), v.end(), [](int &x) { x *= 2; });
+
+    // 출력: 2 4 6 8 10
+    for (auto i : v) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
 }
-// 출력: 2 4 6 8 10
